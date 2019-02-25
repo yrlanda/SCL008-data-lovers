6c1b444971ff17e6b6f6bfd147c6377b1018461c
Yrlanda <yrlaterivero@gmal.com>
2019-02-25 01:54:05 -0300

    se realizan pruebas unitarias

--- test/data.spec.js
+++ test/data.spec.js
@@ -14,3 +14,50 @@ describe('example', () => {
     assert.equal(example(), 'example');
   });
 })
+
+describe('orderData', () => {
+  it('deberia de mostrar al pokemon Abra como el primero en la lista cuando se ordene de la  A - Z', () => {
+    let pokemon_data = window.orderData(data, 'az');
+    assert.equal(pokemon_data[0].name, 'Abra');
+
+  });
+
+  it('deberia de mostrar al pokemon Zubat como el primero en la lista cuando se ordene de la  Z - A', () => {
+    let pokemon_data = window.orderData(data, 'za');
+    assert.equal(pokemon_data[0].name, 'Zubat');
+
+  });
+  it('deberia de mostrar al pokemon Bulbasaur como el primero en la lista cuando se ordena Idasc ', () => {
+    let pokemon_data = window.orderData(data, 'idasc');
+    assert.equal(pokemon_data[0].name, 'Bulbasaur');
+
+  });
+  it('deberia de mostrar al pokemon Mew como el primer pokemon de la lista cuando se ordedne Iddesc', () => {
+    let pokemon_data = window.orderData(data, 'iddesc');
+    assert.equal(pokemon_data[0].name, 'Mew');
+
+  });
+});
+
+describe('filterType', () => {
+  it('deberia de mostrar 14 Pokemons de tipo Grass', () => {
+    let pokemon_data = window.filterType(data, 'Grass');
+    assert.equal(pokemon_data.length, 14);
+
+  });
+  it('deberia de mostrar 3 Pokemons de tipo Ghost', () => {
+    let pokemon_data = window.filterType(data, 'Ghost');
+    assert.equal(pokemon_data.length, 3);
+
+  });
+  it('deberia de mostrar 151 Pokemons de tipo All', () => {
+    let pokemon_data = window.filterType(data, 'all');
+    assert.equal(pokemon_data.length, 151);
+
+  });
+
+});
+
+
+
+
