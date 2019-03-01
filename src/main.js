6df8446b43e00d691c7da99247b9a54ffb2fca4e
Yrlanda <yrlaterivero@gmal.com>
2019-03-01 10:17:23 -0300

    se agrega información especifica en el readme sobre los prototipados y detalles en el main.js y en el data.js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,28 @@
 /* Manejo del DOM */
 window.onload = () => {
-    const data = window.pokemon.pokemon;
-    const pokemon_list = document.getElementById('pokemon');
+  const data = window.pokemon.pokemon;
+  const pokemon_list = document.getElementById('pokemon');
 
-    showCards(data);
-
-
-    const selectType = document.getElementById("type");
-
-    selectType.addEventListener('change', () => {
-        //const pokemon_list = document.getElementById('lista');
-        let condition = selectType.value
-        let filtered = window.filterType(data, condition);
-        pokemon_list.innerHTML = '';
-    });
+  showCards(data);
 
 
-    function showCards(datos) {
-        pokemon_list.innerHTML = '';
-    
-        datos.forEach(pokemon => {
-          pokemon_list.innerHTML += `<div class="card">
+  const selectType = document.getElementById("type");
+
+  selectType.addEventListener('change', () => {
+  
+    let condition = selectType.value
+    let filtered = window.filterType(data, condition);
+    pokemon_list.innerHTML = '';
+
+    showCards(filtered);
+  });
+
+
+  function showCards(datos) {
+    pokemon_list.innerHTML = '';
+
+    datos.forEach(pokemon => {
+      pokemon_list.innerHTML += `<div class="card">
         <img src="${pokemon.img}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${pokemon.name}</h5>
@@ -28,10 +30,10 @@ window.onload = () => {
           <p class="card-text">${pokemon.candy}</p>
         </div>
         </div`;
-    
-        });
-    
-      }
+
+    });
+
+  }
 
 
 
